93d1a573b9bbad53992a251f1f5748378ab1be5a
nierowheezy <olaniyiolabode99@yahoo.com>
2026-03-30 21:05:28 +0100

    reverted back to old code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    }
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://ids-africa-selfcare-portal-api-j5yr.onrender.com",
+        // ↑↑↑ CHANGE THIS to your actual Render backend URL
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
